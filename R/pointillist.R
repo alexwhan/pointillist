69a9b9ae91d3cf605a7c8637ce0c9fb0724c1b68
Alex Whan <alexwhan@gmail.com>
2016-12-09 22:59:49 +1100

    updated docs, added png functionality (maybe?)

--- R/pointillist.R
+++ R/pointillist.R
@@ -1,46 +1,55 @@
 
-#' Make a data frame of jpg data
+#' Make a data frame of img data
 #'
 #' @param file A file path
+#' @param type File type - either "png" or "jpg"
 #'
 #' @return A tidy dataframe describing r, g, b and hex values
 #' @export
 #' @importFrom magrittr %>%
-jpg_df <- function(file) {
-  jpg <- jpeg::readJPEG(file)
+img_df <- function(file, type = NULL) {
+  if(is.null(type)) {
+    ext <- tools::file_ext(file)
+    if(ext %in% c("jpg", "jpeg", "JPG", "JPEG")) {
+      type <- "jpg"
+    } else
+      if(ext %in% c("png", "PNG")) type <- "png"
+  }
+  if(type == "jpg") img <- jpeg::readJPEG(file)
+  if(type == "png") ing <- png::readPNG(file)
 
-  jpg_list <- vector(mode = "list", length = 3)
+  img_list <- vector(mode = "list", length = 3)
 
   vals <- c("r", "g", "b")
 
   for(i in 1:3) {
-    dims <- dim(jpg[,,i])
-    jpg_df <- jpg[,,i] %>%
+    dims <- dim(img[,,i])
+    img_df <- img[,,i] %>%
       as.data.frame() %>%
       dplyr::mutate_("row" = 1:dims[1]) %>%
       tidyr::gather_("col", "value", paste0("V", 1:dims[2]))
-    jpg_df$col <- as.numeric(sub("V", "", col))
-    names(jpg_df)[3] <- vals[i]
-    jpg_list[[i]] <- jpg_df
+    img_df$col <- as.numeric(sub("V", "", img_df$col))
+    names(img_df)[3] <- vals[i]
+    img_list[[i]] <- img_df
   }
 
-  jpg_full <- purrr::reduce(jpg_list, dplyr::left_join)
-  jpg_full$hex <- grDevices::rgb(jpg_full$r,
-                                 jpg_full$g,
-                                 jpg_full$b)
+  img_full <- purrr::reduce(img_list, dplyr::left_join)
+  img_full$hex <- grDevices::rgb(img_full$r,
+                                 img_full$g,
+                                 img_full$b)
 
-  return(jpg_full)
+  return(img_full)
 }
 
 
 #' Set colour depth to either 8 or 24 bit
 #'
-#' @param jpg_df A tidy data frame - output from \code{jpg_df()}
+#' @param img_df A tidy data frame - output from \code{img_df()}
 #' @param depth Numeric, describing colour depth - either 8 or 24 (bit)
 #'
 #' @return A tidy data frame with colour depth adjusted
 #' @export
-colour_depth <- function(jpg_df, depth) {
+colour_depth <- function(img_df, depth) {
   if(!depth %in% c(8, 24)) {
     stop("The depth argument must be either 8 or 24")
   }
@@ -51,50 +60,59 @@ colour_depth <- function(jpg_df, depth) {
     rg_vals <- 255
     b_vals <- 255
   }
-  jpg_df$r <- round(jpg_df$r * rg_vals, 0) / rg_vals
-  jpg_df$g <- round(jpg_df$g * rg_vals, 0) / rg_vals
-  jpg_df$b <- round(jpg_df$b * b_vals, 0) / b_vals
-  jpg_df$hex <- grDevices::rgb(jpg_df$r,
-                    jpg_df$g,
-                    jpg_df$b)
-  return(jpg_df)
+  img_df$r <- round(img_df$r * rg_vals, 0) / rg_vals
+  img_df$g <- round(img_df$g * rg_vals, 0) / rg_vals
+  img_df$b <- round(img_df$b * b_vals, 0) / b_vals
+  img_df$hex <- grDevices::rgb(img_df$r,
+                    img_df$g,
+                    img_df$b)
+  return(img_df)
 }
 
-#' Randomly sample a jpg df to reduce points
+#' Randomly sample a img df to reduce points
 #'
-#' @param jpg_df A tidy data frame - output from \code{jpg_df()}
+#' @param img_df A tidy data frame - output from \code{img_df()}
 #' @param density Numeric, controls how many points are included
 #'
 #' @return A tidy data frame with a reduced number of rows
 #' @export
-sample_df <- function(jpg_df, density) {
-  n <- ceiling(nrow(jpg_df) * density)
-  jpg_df <- jpg_df[sample(nrow(jpg_df), n, replace = FALSE)]
-  return(jpg_df)
+sample_df <- function(img_df, density) {
+  n <- ceiling(nrow(img_df) * density)
+  img_df <- img_df[sample(nrow(img_df), n, replace = FALSE),]
+  return(img_df)
 }
 
 #' Make a pointillist ggplot2 object
 #'
-#' @param jpg_df A tidy data frame - output from \code{jpg_df()}
+#' @param img_df A tidy data frame - output from \code{img_df()}
 #' @param depth Numeric, describing colour depth - either 8 or 24 (bit)
 #' @param density Numeric, controls how many points are included
+#' @param point_range Numeric vector defining the min and max point size
 #'
 #' @return a ggplot2 object
 #' @export
-pointillise <- function(jpg_df, depth = 8, density = 0.1) {
-  if(!all(names(jpg_df) == c("row", "col", "r", "g", "b", "hex"))) {
-    stop("The jpg dataframe doesn't look right. It should be output from jpg_df()")
+pointillise <- function(img_df, depth = 8, density = 0.1, point_range = c(1, 2)) {
+  if(!all(names(img_df) == c("row", "col", "r", "g", "b", "hex"))) {
+    stop("The img dataframe doesn't look right. It should be output from img_df()")
   }
-  if(max(c(jpg_df$r, jpg_df$g, jpg_df$b)) > 1) {
-    stop("The colour range in the jpg dataframe is too big. The dataframe should be output from jpg_df()")
+  if(max(c(img_df$r, img_df$g, img_df$b)) > 1) {
+    stop("The colour range in the img dataframe is too big. The dataframe should be output from img_df()")
   }
 
-  jpg_df <- colour_depth(jpg_df, depth)
+  img_df <- colour_depth(img_df, depth)
 
-  coord_ratio <- max(jpg_df$row) / max(jpg_df$col)
+  img_df <- sample_df(img_df, density)
 
-  p_out <- ggplot2::ggplot(jpg_df, ggplot2::aes_string("col", "row")) +
-    ggplot2::geom_point(colour = jpg_df$hex) +
+  coord_ratio <- max(img_df$row) / max(img_df$col)
+
+  sizes <- sample(seq(from = point_range[1],
+                      to = point_range[2],
+                      by = 0.1),
+                nrow(img_df),
+                replace = TRUE)
+
+  p_out <- ggplot2::ggplot(img_df, ggplot2::aes_string("col", "row")) +
+    ggplot2::geom_point(colour = img_df$hex, size = sizes) +
     ggplot2::guides(colour = FALSE) +
     ggplot2::scale_y_reverse(expand = c(0, 0)) +
     ggplot2::scale_x_continuous(expand = c(0, 0)) +
